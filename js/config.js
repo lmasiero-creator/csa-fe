673ffbb0c705182af5cd652f9593504eb076c393
l.masiero <l.masiero@almaviva.it>
2026-03-31 01:37:41 +0200

    fix: remove trailing slash from API_BASE_URL (was causing //api/ double-slash paths)

--- js/config.js
+++ js/config.js
@@ -16,4 +16,4 @@ export const BASE_PATH = isLocal ? '' : '/csa-fe';
 /** URL of the Express backend. */
 export const API_BASE_URL = isLocal
   ? 'http://localhost:3000'
-  : 'https://csa-be-ug3p.onrender.com/';
+  : 'https://csa-be-ug3p.onrender.com';
